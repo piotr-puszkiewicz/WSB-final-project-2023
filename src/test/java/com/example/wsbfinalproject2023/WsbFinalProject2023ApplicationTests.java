d67b8733201396ee8df3ea5250468dc61f2e9a3f
piotr-puszkiewicz <puszkiewicz.p@gmail.com>
2023-01-22 00:36:17 +0100

    Podstawowa wersja aplikacji Spring Boot

---
+++ src/test/java/com/example/wsbfinalproject2023/WsbFinalProject2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsbfinalproject2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbFinalProject2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
